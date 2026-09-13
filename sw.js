5f9036531436513ec075d349b07a03e5aab6b419
ankushchhajed7-cmd <ankushchhajed7@gmail.com>
2026-09-13 22:33:15 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Voice v1.01 - service worker */
-const CACHE = 'voice-v1-02';
+const CACHE = 'voice-v1-04';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
